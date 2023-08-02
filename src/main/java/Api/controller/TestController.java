9374293540980404e55a4401db614ef8b2954382
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2023-08-02 20:36:31 +0700

    add api fibonacy

--- src/main/java/Api/controller/TestController.java
+++ src/main/java/Api/controller/TestController.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.*; 
+import java.lang.reflect.Array;
+import static org.junit.Assert.*;
 
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/test")
+@RequestMapping("/api/test")
 public class TestController {
 
 	@RequestMapping(value = "count-data/{x}/{y}")
@@ -17,6 +20,36 @@ public class TestController {
 	    Integer count;
 	    count = x+y;
 		return count;
+	}
+	
+	@RequestMapping(value = "fibonacy/{x}")
+	@ResponseBody
+	public ArrayList<Integer> fibonacy(@PathVariable Integer x) {
+		
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		    
+		for(int i = 0; i < x; i++) {		
+			if (i < 2) {
+				result.add(1);
+			} else {
+				int before1 = result.get(i-1);
+				int before2 = result.get(i-2);
+				
+				int value = before1 + before2;
+				result.add(value);
+			}
+		}
+
+		int n = 3;
+
+		System.out.print("Bilangan fibonacci pada posisi " + n + " adalah ");
+		// get fibonacci pada posisi ke-4
+		System.out.println(result.get(n));
+
+		System.out.println("\nBarisan fibonacci " + x + " bilangan pertama:");
+		// print semua ArrayList
+		result.forEach((e) -> System.out.println(e));
+		return result;
 	}
 	
 }
